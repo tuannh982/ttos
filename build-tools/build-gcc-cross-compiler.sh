1207205bbbfb6a7e4caae8b7520b2eeedf6094f1
tuannh982 <tuannh982@gmail.com>
2022-12-29 21:20:20 +0700

    add cross compiler target

---
+++ build-tools/build-gcc-cross-compiler.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+script_dir() {
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+        SOURCE=$(readlink "$SOURCE")
+        [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    echo $( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+}
+
+SCRIPT_DIR=$(script_dir)
+
+IFS=' ' read -ra TARGETS <<< $(cat "$SCRIPT_DIR/TARGETS.txt")
+for TARGET in "${TARGETS[@]}"; do
+    echo "building $TARGET cross compiler"
+    docker build $SCRIPT_DIR/gcc-cross-compiler --build-arg TARGET=$TARGET -t gcc-cross-compiler:$TARGET
+done
